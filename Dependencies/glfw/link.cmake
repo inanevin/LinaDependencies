580732a75045fea14c1b652860d796a25a77ab74
Inan Evin <inanevin@gmail.com>
2022-06-11 17:22:57 +0300

    glfw glad freetype

---
+++ Dependencies/glfw/link.cmake
@@ -0,0 +1,12 @@
+set(DIRNAME "glfw")
+set(LIBNAME "glfw3")
+
+target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/Dependencies/${DIRNAME}/include)
+
+if(WIN32)
+   target_link_libraries(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME}.lib)
+endif(WIN32)
+
+if(UNIX)
+   target_link_libraries(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME}.a)
+endif(UNIX)
