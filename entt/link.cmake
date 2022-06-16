e83bd8e548c9bb434c77316e582acc73915bf3cf
Inan Evin <inanevin@gmail.com>
2022-06-16 22:34:01 +0300

    sync

--- entt/link.cmake
+++ entt/link.cmake
@@ -1,7 +1,7 @@
 macro(link_entt)
 
    set(LIBNAME "entt")
-   set(DIRNAME FALSE"entt")
+   set(DIRNAME "entt")
    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
    message("${PROJECT_NAME} -> ${LIBNAME} has been linked.")
 endmacro()
