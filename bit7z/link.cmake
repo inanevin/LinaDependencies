e855b828ac98f3498a78a55dc6fd5fb6442d8ffd
Inan Evin <inanevin@gmail.com>
2022-06-16 23:31:50 +0300

    sync

--- bit7z/link.cmake
+++ bit7z/link.cmake
@@ -1,17 +1,18 @@
 macro(link_bit7z)
 
    set(DIRNAME "bit7z")
-   set(LIBNAME "bit7z64")
+   set(LIBNAME_DBG "bit7z64_d")
+   set(LIBNAME_REL "bit7z64")
 
    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
 
    if(WIN32)
-      target_link_libraries(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME}.lib)
+      target_link_libraries(${PROJECT_NAME} PUBLIC debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/Debug/${LIBNAME_DBG}.lib optimized ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/Release/${LIBNAME_REL}.lib)
    endif(WIN32)
 
    if(UNIX)
-      target_link_libraries(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME}.a)
+   target_link_libraries(${PROJECT_NAME} PUBLIC debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/Debug/${LIBNAME_DBG}.a optimized ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/Release/${LIBNAME_REL}.a)
    endif(UNIX)
 
-   message("${PROJECT_NAME} -> ${LIBNAME} has been linked.")
+   message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
 endmacro()
