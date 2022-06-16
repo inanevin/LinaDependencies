5bdfaa1b704245e1bfbf480631e4f3e666a78d92
Inan Evin <inanevin@gmail.com>
2022-06-16 22:55:22 +0300

    sync

--- assimp/link.cmake
+++ assimp/link.cmake
@@ -6,13 +6,7 @@ macro(link_assimp)
 
    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
 
-   if(WIN32)
-      target_link_libraries(${PROJECT_NAME} PUBLIC Debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/Debug/${LIBNAME_DBG}.lib Release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/Release/${LIBNAME_RLS}.lib)
-   endif(WIN32)
-
-   if(UNIX)
-      target_link_libraries(${PROJECT_NAME} PUBLIC Debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/Debug/${LIBNAME_DBG}.a Release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/Release/${LIBNAME_RLS}.a)
-   endif(UNIX)
+  
 
    message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
 endmacro()
