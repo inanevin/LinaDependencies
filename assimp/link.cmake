2743ecba1cc875408d8e69a23f901c44b0dffaf8
Inan Evin <inanevin@gmail.com>
2022-06-16 22:53:19 +0300

    sync

--- assimp/link.cmake
+++ assimp/link.cmake
@@ -7,11 +7,11 @@ macro(link_assimp)
    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
 
    if(WIN32)
-      target_link_libraries(${PROJECT_NAME} debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME_DBG}.lib release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME_RLS}.lib)
+      target_link_libraries(${PROJECT_NAME} PUBLIC debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME_DBG}.lib release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Win64/$<CONFIGURATION>/${LIBNAME_RLS}.lib)
    endif(WIN32)
 
    if(UNIX)
-      target_link_libraries(${PROJECT_NAME} debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME_DBG}.a release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME_RLS}.a)
+      target_link_libraries(${PROJECT_NAME} PUBLIC debug ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME_DBG}.a release ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/Unix64/$<CONFIGURATION>/${LIBNAME_RLS}.a)
    endif(UNIX)
 
    message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
