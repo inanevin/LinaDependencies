1ee331f20f892f0202035c3ab3e974edb6aa8aa8
Inan Evin <inanevin@gmail.com>
2022-08-10 19:40:39 +0300

    natvis test

--- phmap/link.cmake
+++ phmap/link.cmake
@@ -4,5 +4,6 @@ macro(link_phmap)
 
    target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
 
+   add_custom_target(NatVis SOURCES ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/phmap.natvis)
    message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
 endmacro()
