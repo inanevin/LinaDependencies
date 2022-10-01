13af39f1650919dd0889e6b4f885eb23464fdef9
Inan Evin <inanevin@gmail.com>
2022-10-01 17:57:00 +0300

    remove icon font headers

--- iconfontheaders/link.cmake
+++
@@ -1,5 +0,0 @@
-macro(link_iconfontheaders)
-   set(DIRNAME "iconfontheaders")
-   target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
-   message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
-endmacro()
