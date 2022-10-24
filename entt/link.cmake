f64e38a7a9a715bf49e2f7ed159fdb04a300f110
Inan Evin <inanevin@gmail.com>
2022-10-24 21:24:11 +0300

    remove entt & glfw

--- entt/link.cmake
+++
@@ -1,7 +0,0 @@
-macro(link_entt)
-
-   set(LIBNAME "entt")
-   set(DIRNAME "entt")
-   target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
-   message("${PROJECT_NAME} -> ${LIBNAME} has been linked.")
-endmacro()
