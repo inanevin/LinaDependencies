fab7d07558d0a8a3d6374254163a329d291b7065
Inan Evin <inanevin@gmail.com>
2022-12-04 17:53:04 +0200

    remove unused

--- cereal/link.cmake
+++
@@ -1,9 +0,0 @@
-macro(link_cereal)
-
-   set(DIRNAME "cereal")
-   set(LIBNAME "cereal")
-
-   target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/_Dependencies/${DIRNAME}/include)
-
-   message("${PROJECT_NAME} -> ${LIBNAME} has been linked.")
-endmacro()
