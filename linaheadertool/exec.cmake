b6cd601f47f1b2f8dcd258b713b70330008780c2
Inan Evin <inanevin@gmail.com>
2023-07-30 14:56:04 +0300

    sync

--- linaheadertool/exec.cmake
+++
@@ -1,9 +0,0 @@
-# Launch Lina Header tool prior to building to prepare the reflection data for runtime.
-macro(execute_linaheadertool)
-
-add_custom_command(
-TARGET ${PROJECT_NAME}
-PRE_BUILD
-COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_SOURCE_DIR}/_Dependencies/linaheadertool/bin/Win64/LinaHeader.exe)
-
-endmacro()
